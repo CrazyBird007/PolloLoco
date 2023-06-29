a5b3f25311fc120c5bdf081fe714d4f096632eb4
Albert <wissigkeit@outlook.de>
2023-06-29 16:55:43 +0200

    save bevor adding salsa bottle

--- classes/character.class.js
+++ classes/character.class.js
@@ -61,7 +61,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/1_idle/long_idle/I-20.png',
     ];
     world; // hier werden alle variablen aus der world class gespeichert //  durch die setWorld() func. aus der world class
-
+    isCharacterDead = false;
 
     constructor() {
         super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
@@ -78,7 +78,6 @@ class Character extends MovableObject {
 
 
     animate() {
-        let i = 0;
         setInterval(() => {
             if (this.world.keyboard.RIGHT && this.posX < this.world.level.level_end_x) { // char kann nicht weiter als level_endX nach rechts gehen
                 this.moveRight();
@@ -95,13 +94,13 @@ class Character extends MovableObject {
             this.world.camera_x = -this.posX + 150; // wenn x vom char. verändert wird, wird die camera_x variable verändert, so bewegt sich das bild
         }, 1000 / 60);   // durch das + wird der charakter im bild verschoben, so könnte er mittig sein oder leicht links
         setInterval(() => {
-            if (this.isDead() && i < 10) {
+            if (this.isDead() && !this.isCharacterDead) {
                 this.playAnimation(this.IMAGES_DEAD);
-                i++
-            } else {
-                console.log('character tot');
-                this.loadImage('./img/9_intro_outro_screens/game_over/game over.png');
-            } if (this.isHurt()) {
+                this.isCharacterDead = true;
+                setTimeout(() => {
+                    document.getElementById('looseGame').classList.remove('d-none');
+                }, 2000);
+            } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
             } else if (this.isAboveGround()) {
                 this.playAnimation(this.IMAGES_JUMPING);
