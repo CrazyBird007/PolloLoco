dad6907e68f390c40feeadce9b03cb94d13e261a
Albert <wissigkeit@outlook.de>
2023-06-29 14:36:58 +0200

    add idle and longidle

--- classes/character.class.js
+++ classes/character.class.js
@@ -48,6 +48,18 @@ class Character extends MovableObject {
         './img/2_character_pepe/1_idle/idle/I-9.png',
         './img/2_character_pepe/1_idle/idle/I-10.png',
     ];
+    IMAGES_LONG_IDLE = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png',
+    ];
     world; // hier werden alle variablen aus der world class gespeichert //  durch die setWorld() func. aus der world class
 
 
@@ -58,6 +70,7 @@ class Character extends MovableObject {
         this.loadImages(this.IMAGES_DEAD);
         this.loadImages(this.IMAGES_HURT);
         this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_LONG_IDLE);
         this.applyGravity();
         this.animate();
         this.isDead();
@@ -80,19 +93,29 @@ class Character extends MovableObject {
 
             this.world.camera_x = -this.posX + 150; // wenn x vom char. verändert wird, wird die camera_x variable verändert, so bewegt sich das bild
         }, 1000 / 60);   // durch das + wird der charakter im bild verschoben, so könnte er mittig sein oder leicht links
-        setInterval(() => {    
-            if (this.isDead() ) {
+        setInterval(() => {
+            if (this.isDead()) {
                 this.playAnimation(this.IMAGES_DEAD);
-            } else if(this.isHurt()) {
+            } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
             } else if (this.isAboveGround()) {
                 this.playAnimation(this.IMAGES_JUMPING);
+                this.lastActiveTime = new Date();
+            } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {  // die charakter animation wird nur abgespielt wenn man die taste nach rechts/links drückt
+                this.playAnimation(this.IMAGES_WALKING);
+                this.lastActiveTime = new Date();
             } else {
-                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {  // die charakter animation wird nur abgespielt wenn man die taste nach rechts drückt
-                    this.playAnimation(this.IMAGES_WALKING);
+                const currentTime = new Date();
+                const idleTime = currentTime - this.lastActiveTime;
+                const startLongIdle = 3000; //long idle after 10 sec.
+            
+                if (idleTime >= startLongIdle) {
+                    this.playAnimation(this.IMAGES_LONG_IDLE);
+                } else {
+                    this.playAnimation(this.IMAGES_IDLE);
                 }
             }
-        }, 50);
+        }, 150);
     }
 
     jump() {
